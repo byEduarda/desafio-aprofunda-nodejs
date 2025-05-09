cbabc10e9e1cd86539d9b5888b4013857b5035e8
byEduarda <albuquerque.zxl@gmail.com>
2025-05-09 20:27:11 -0300

    primeiro exercicio

---
+++ m_eduarda_albuqueque.js
@@ -0,0 +1,12 @@
+//Maria Eduarda Albuquerque da Silva
+
+
+function ImprimePares(valor) {
+    for(let valores = 0; i <= valores; valores++){
+        if(valores % 2 === 0){
+            console.log(valores)
+        }
+    }
+}
+
+ImprimePares(10)
