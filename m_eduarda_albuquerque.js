d65689303a5363ddf357165776df03dd41b67bdd
byEduarda <albuquerque.zxl@gmail.com>
2025-05-10 12:10:53 -0300

    Update m_eduarda_albuquerque.js

--- m_eduarda_albuquerque.js
+++ m_eduarda_albuquerque.js
@@ -5,7 +5,16 @@ function ImprimePares(n){
         if(i % 2 === 0) console.log(i)  //testa quais numeros sao pares e imprime quando o resultado for true
     }
 }
-
+console.log("Resultado do exercício número um:")
 ImprimePares(10) 
 
 
+
+//2️⃣Soma dos Números de um Array
+
+function somaArray(arr){
+    return arr.reduce((acc, num) => acc + num, 0);
+}
+
+console.log("\nResultado do exercicío número dois:")
+console.log(somaArray([1, 2, 5, 1]))
