f0641429429ce5d82bcab488955424d7323429ad
byEduarda <albuquerque.zxl@gmail.com>
2025-05-10 12:54:26 -0300

    Update m_eduarda_albuquerque.js

--- m_eduarda_albuquerque.js
+++ m_eduarda_albuquerque.js
@@ -37,3 +37,15 @@ console.log(ePrimo(10));
 
 //4️⃣FizzBuzz
 
+function FizzBuzz(nms){
+    for(let i = 0; i <= 50; i++){
+        if(i % 3 === 0 && i % 5 === 0) {console.log("fizzBuzz");
+            }else if(i % 3 === 0){
+                console.log("fizz");
+            }else if(i % 5 === 0){
+                console.log("buzz");
+            }else{
+                console.log(i);
+            }
+    }
+}
