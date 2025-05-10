0d202910121ff787bef03b9a652a12671bca57a8
byEduarda <albuquerque.zxl@gmail.com>
2025-05-10 11:07:18 -0300

    Update m_eduarda_albuquerque.js

--- m_eduarda_albuquerque.js
+++ m_eduarda_albuquerque.js
@@ -1,11 +1,11 @@
 //1️⃣Imprimindo Números Pares
 
-function n_Pares(n){
+function ImprimePares(n){
     for(let i = 0; i <= n; i++){   //usa o indice 'i' para percorrer os numeros ate 'n'
         if(i % 2 === 0) console.log(i)  //testa quais numeros sao pares e imprime quando o resultado for true
     }
 }
 
-n_Pares(10) 
+ImprimePares(10) 
 
 
