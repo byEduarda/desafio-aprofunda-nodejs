7a6fd3faa7415c75481ed58b00f7d7f51ee9e684
byEduarda <albuquerque.zxl@gmail.com>
2025-05-10 11:04:27 -0300

    comentários

--- m_eduarda_albuquerque.js
+++ m_eduarda_albuquerque.js
@@ -1,7 +1,7 @@
 function n_Pares(n){
-    for(let i = 0; i <= n; i++){
-        if(i % 2 === 0) console.log(i)
+    for(let i = 0; i <= n; i++){   //usa o indice 'i' para percorrer os numeros ate 'n'
+        if(i % 2 === 0) console.log(i)  //testa quais numeros sao pares e imprime quando o resultado for true
     }
 }
 
-n_Pares(10)
+n_Pares(10) 
